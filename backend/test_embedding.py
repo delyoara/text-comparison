afa05ae38b8b7ca9b71a1de4a9baeb1e77a59214
petronela <delyoara@yahoo.fr>
2025-08-08 06:00:15 +0200

    Initial commit

---
+++ backend/test_embedding.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+
+emb1 = model.encode("Bonjour tout le monde", convert_to_tensor=True)
+emb2 = model.encode("Hello world", convert_to_tensor=True)
+
+score = util.cos_sim(emb1, emb2)
+print("Similarité :", score.item())
+
+
